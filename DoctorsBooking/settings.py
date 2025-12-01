59d8167df87cc376e09aeb9f272f014a8e2f9341
Cheecheez101 <michaelnganga678@gmail.com>
2025-12-01 16:26:14 +0300

    deploying

--- DoctorsBooking/settings.py
+++ DoctorsBooking/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 # settings.py
 import dj_database_url
-
+from dotenv import load_dotenv
 import os # You usually need this import at the top
 # settings.py
 
@@ -31,19 +31,24 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # ]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zk8f!44s%1)(t^iw*@^j2=7)rqsb0yqd9be-v=e_ye%auw8@p3'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1")
 
-ALLOWED_HOSTS = []
+# Allow multiple hosts via comma-separated env var
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
